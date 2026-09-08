323aab2b003126d9ab517bd910485598d593e601
mehmed-faheim-arslan <mehmedfaheimarslan@gmail.com>
2026-09-08 22:14:39 +0100

    fixed mem leak

--- c/exercises/basics/str_reverser.c
+++ c/exercises/basics/str_reverser.c
@@ -1,14 +1,18 @@
 #include "str_reverser.h"
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 void reverse(const char *str) {
-  int len = strlen(str);
+  size_t len = strlen(str);
   char *op = malloc(len + 1);
+  if (!op)
+    return;
   for (int i = 0; i < len; i++) {
     op[i] = str[len - i - 1];
   }
   op[len] = '\0';
   printf("%s\n", op);
+  free(op);
 }
